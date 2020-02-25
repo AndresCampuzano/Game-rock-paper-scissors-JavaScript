25267cdc0d1354f960cd73aa96fdab3cbe0192c1
Andres Campuzano <campuxano88@gmail.com>
2020-02-25 08:56:09 -0500

    addEventListener.

--- game.js
+++ game.js
@@ -1,59 +1,64 @@
 // Rock, paper, scissors game
-function start() { 
-    var playerOne = prompt("Write rock, paper or scissors:");
-    
-    if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
-        function game(playerOne) {
-            var x = Math.round(Math.random() * 10); // getting a random number between 1 and 10
-            // if 0-3 = rock
-            // if 4-6 = paper
-            // if 7-10 = scissors
-            if (x <= 3) {
-                var machinePlayer = "rock";
-                if (playerOne === "paper") {
-                    alert(`✅ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                } else if (playerOne === "scissors") {
-                    alert(`❌ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                } else if (playerOne === machinePlayer) {
-                    alert(`DRAW ⚠ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                }
-            } else if (x <= 6) {
-                var machinePlayer = "paper";
-                if (playerOne === "rock") {
-                    alert(`❌ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                } else if (playerOne === "scissors") {
-                    alert(`✅ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                } else if (playerOne === machinePlayer) {
-                    alert(`DRAW ⚠ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                }
-            } else {
-                var machinePlayer = "scissors";
-                if (playerOne === "rock") {
-                    alert(`✅ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                } else if (playerOne === "paper") {
-                    alert(`❌ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                } else if (playerOne === machinePlayer) {
-                    alert(`DRAW ⚠ you: ${playerOne}, machine: ${machinePlayer}`);
-                    start();
-                }
-                
-            }
-        }
-        
-        game(playerOne);
-    
-    } else {
-        alert("Please write rock, paper or scissors");
-        start();
-    }
 
-}    
-start();
+//getting id from HTML
+document.getElementById("rock").addEventListener("click", rockFunction);
+document.getElementById("paper").addEventListener("click", paperFunction);
+document.getElementById("scissors").addEventListener("click", scissorsFunction);
+
+//turning id in strings
+function rockFunction() {
+    var playerOne = "rock";
+    return game(playerOne);
+}
+function paperFunction() {
+    var playerOne = "paper";
+    return game(playerOne);
+}
+function scissorsFunction() {
+    var playerOne = "scissors";
+    return game(playerOne);
+}
+
+function game(playerOne) {
+    var x = Math.round(Math.random() * 8); // getting a random number between 0 and 8
+    // if 0-2 = rock
+    // if 3-5 = paper
+    // if 6-8 = scissors
+    if (x <= 2) {
+        var machinePlayer = "rock";
+        if (playerOne === "paper") {
+            console.log(`✅ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        } else if (playerOne === "scissors") {
+            console.log(`❌ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        } else if (playerOne === machinePlayer) {
+            console.log(`DRAW ⚠ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        }
+    } else if (x <= 5) {
+        var machinePlayer = "paper";
+        if (playerOne === "rock") {
+            console.log(`❌ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        } else if (playerOne === "scissors") {
+            console.log(`✅ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        } else if (playerOne === machinePlayer) {
+            console.log(`DRAW ⚠ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        }
+    } else {
+        var machinePlayer = "scissors";
+        if (playerOne === "rock") {
+            console.log(`✅ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        } else if (playerOne === "paper") {
+            console.log(`❌ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        } else if (playerOne === machinePlayer) {
+            console.log(`DRAW ⚠ you: ${playerOne}, machine: ${machinePlayer}`);
+            // start();
+        }
+    }
+} 
